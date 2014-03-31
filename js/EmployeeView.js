67b5a4d2504aa901d81934abc5e6c3dc84ca4281
Juhana Soini <juhana.soini@avenis.fi>
2014-03-31 22:09:40 +0300

    Second commit

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -15,11 +15,9 @@ var EmployeeView = function(adapter, template, employee) {
         console.log( "OKI" );
         navigator.geolocation.getCurrentPosition(
             function(position) {
-                alert(position.coords.latitude + ',' + position.coords.longitude);
                 console.log( position.coords.latitude + ',' + position.coords.longitude );
             },
             function() {
-                alert('Error getting location');
                 console.log('Error getting location');
             });
         return false;
